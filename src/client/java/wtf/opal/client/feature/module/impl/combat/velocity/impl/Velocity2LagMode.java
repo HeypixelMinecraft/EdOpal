9f83bc924bf29176171cd0324a7c6c0cef0ffa4d
HeypixelMinecraft <github@example.com>
2026-07-17 19:47:00 +0800

    Fix Velocity2 GrimAC bypass - add VelocityUpdateEvent handler and reduce bread delay

--- src/client/java/wtf/opal/client/feature/module/impl/combat/velocity/impl/Velocity2LagMode.java
+++ src/client/java/wtf/opal/client/feature/module/impl/combat/velocity/impl/Velocity2LagMode.java
@@ -8,11 +8,14 @@ import wtf.opal.client.feature.module.impl.combat.velocity.Velocity2Mode;
 import wtf.opal.client.feature.module.impl.combat.velocity.Velocity2Module;
 import wtf.opal.client.feature.module.property.impl.bool.BooleanProperty;
 import wtf.opal.client.feature.module.property.impl.number.BoundedNumberProperty;
+import wtf.opal.client.feature.module.property.impl.number.NumberProperty;
 import wtf.opal.event.impl.game.PreGameTickEvent;
 import wtf.opal.event.impl.game.input.MoveInputEvent;
 import wtf.opal.event.impl.game.packet.ReceivePacketEvent;
 import wtf.opal.event.impl.game.packet.SendPacketEvent;
+import wtf.opal.event.impl.game.player.movement.knockback.VelocityUpdateEvent;
 import wtf.opal.event.subscriber.Subscribe;
+import wtf.opal.mixin.LivingEntityAccessor;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -21,20 +24,19 @@ import static wtf.opal.client.Constants.mc;
 
 public final class Velocity2LagMode extends Velocity2Mode {
 
-    private final BoundedNumberProperty lagTime = new BoundedNumberProperty("LagTime", "ticks", 5, 5, 1, 20, 1).hideIf(() -> this.module.getActiveMode() != this);
-    private final BoundedNumberProperty breadDelay = new BoundedNumberProperty("BreadDelay", "ticks", 3, 3, 0, 10, 1).hideIf(() -> this.module.getActiveMode() != this);
-    private final BoundedNumberProperty breadCount = new BoundedNumberProperty("BreadCount", "packets", 2, 2, 1, 5, 1).hideIf(() -> this.module.getActiveMode() != this);
+    private final NumberProperty horizontal = new NumberProperty("Horizontal", "%", 0, 0, 100, 1).hideIf(() -> this.module.getActiveMode() != this);
+    private final NumberProperty vertical = new NumberProperty("Vertical", "%", 100, 0, 100, 1).hideIf(() -> this.module.getActiveMode() != this);
+    private final BoundedNumberProperty breadDelay = new BoundedNumberProperty("BreadDelay", "ticks", 1, 1, 0, 5, 1).hideIf(() -> this.module.getActiveMode() != this);
+    private final BoundedNumberProperty breadCount = new BoundedNumberProperty("BreadCount", "packets", 2, 2, 0, 3, 1).hideIf(() -> this.module.getActiveMode() != this);
     private final BooleanProperty jumpReset = new BooleanProperty("JumpReset", false).hideIf(() -> this.module.getActiveMode() != this);
     private final BooleanProperty considerExplosion = new BooleanProperty("ConsiderExplosion", true).hideIf(() -> this.module.getActiveMode() != this);
-    private final BooleanProperty delayBread = new BooleanProperty("DelayBread", true).hideIf(() -> this.module.getActiveMode() != this);
 
     public Velocity2LagMode(Velocity2Module module) {
         super(module);
-        module.addProperties(lagTime, breadDelay, breadCount, jumpReset, considerExplosion, delayBread);
+        module.addProperties(horizontal, vertical, breadDelay, breadCount, jumpReset, considerExplosion);
     }
 
     private final BlockHolder inboundBlockHolder = new BlockHolder(InboundNetworkBlockage.get());
-
     private final Deque<net.minecraft.network.packet.c2s.common.CommonPongC2SPacket> queuedPongs = new ArrayDeque<>();
 
     private boolean shouldLag = false;
@@ -46,6 +48,9 @@ public final class Velocity2LagMode extends Velocity2Mode {
     private int breadsToDelay = 0;
     private int breadsDelayed = 0;
 
+    private boolean velocityReceived = false;
+    private int velocityTick = 0;
+
     @Subscribe
     public void onReceivePacket(final ReceivePacketEvent event) {
         if (mc.player == null) return;
@@ -53,13 +58,19 @@ public final class Velocity2LagMode extends Velocity2Mode {
         if (event.getPacket() instanceof EntityVelocityUpdateS2CPacket velocity) {
             if (velocity.getEntityId() == mc.player.getId()) {
                 if (!isExplosionVelocity(velocity) || considerExplosion.getValue()) {
-                    shouldLag = true;
-                    lagTicks = lagTime.getValue().first.intValue();
+                    velocityReceived = true;
+                    velocityTick = mc.player.age;
 
-                    if (delayBread.getValue()) {
+                    int bd = breadDelay.getValue().first.intValue();
+                    int bc = breadCount.getValue().first.intValue();
+
+                    if (bd > 0 && bc > 0) {
+                        shouldLag = true;
+                        lagTicks = bd;
+
                         breadDelayed = true;
-                        breadDelayTicks = breadDelay.getValue().first.intValue();
-                        breadsToDelay = breadCount.getValue().first.intValue();
+                        breadDelayTicks = bd;
+                        breadsToDelay = bc;
                         breadsDelayed = 0;
                     }
                 }
@@ -74,6 +85,32 @@ public final class Velocity2LagMode extends Velocity2Mode {
             queuedPongs.add(pong);
             breadsDelayed++;
         }
+    }
+
+    @Subscribe
+    public void onVelocityUpdate(final VelocityUpdateEvent event) {
+        if (mc.player == null) {
+            return;
+        }
+
+        if (event.isExplosion() && !considerExplosion.getValue()) {
+            return;
+        }
+
+        final double h = horizontal.getValue() / 100;
+        final double v = vertical.getValue() / 100;
+
+        event.setCancelled();
+
+        if (h == 0 && v == 0) {
+            return;
+        }
+
+        mc.player.setVelocity(
+                event.getVelocityX() * h,
+                event.getVelocityY() * v,
+                event.getVelocityZ() * h
+        );
     }
 
     @Subscribe
@@ -108,11 +145,16 @@ public final class Velocity2LagMode extends Velocity2Mode {
         if (!shouldLag && !breadDelayed) {
             inboundBlockHolder.release();
         }
+
+        if (velocityReceived && mc.player.age - velocityTick > 20) {
+            velocityReceived = false;
+        }
     }
 
     @Subscribe
     public void onMoveInput(final MoveInputEvent event) {
         if (jumpReset.getValue() && shouldJump && mc.player.isOnGround() && mc.player.isSprinting()) {
+            ((LivingEntityAccessor) mc.player).setJumpingCooldown(0);
             event.setJump(true);
             shouldJump = false;
         }
@@ -125,10 +167,12 @@ public final class Velocity2LagMode extends Velocity2Mode {
     private void resetAll() {
         shouldLag = false;
         breadDelayed = false;
+        velocityReceived = false;
         lagTicks = 0;
         breadDelayTicks = 0;
         breadsToDelay = 0;
         breadsDelayed = 0;
+        velocityTick = 0;
         shouldJump = false;
         queuedPongs.clear();
         inboundBlockHolder.release();
@@ -142,7 +186,9 @@ public final class Velocity2LagMode extends Velocity2Mode {
 
     @Override
     public String getSuffix() {
-        return "Lag " + lagTime.getValue().first.intValue() + "t " + (delayBread.getValue() ? "Bread " + breadDelay.getValue().first.intValue() + "t " + breadCount.getValue().first.intValue() + "x" : "");
+        int bd = breadDelay.getValue().first.intValue();
+        int bc = breadCount.getValue().first.intValue();
+        return horizontal.getValue().intValue() + " " + vertical.getValue().intValue() + (bd > 0 ? " Bread " + bd + "t " + bc + "x" : "");
     }
 
     @Override
